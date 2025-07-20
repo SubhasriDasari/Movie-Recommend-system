b67912772bf73d13e7b363b5cd5b6e32f6d9f1f3
Dasari.Subhasri <subhasridasari.01@gmail.com>
2025-07-20 22:02:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-AUTHOR_NAME = "SUBHASRI DASARI"
-AUTHOR_EMAIL = "subhasridasari.01@gmail.com"  # <-- fix email format
-SRC_REPO = 'src'
-LIST_OF_REQUIREMENTS = ['streamlit']
-
 setup(
-    name=SRC_REPO,
+    name='movie_recommender',
     version='0.0.1',
-    author=AUTHOR_NAME,
-    author_email=AUTHOR_EMAIL,
+    author='SUBHASRI DASARI',
+    author_email='subhasridasari.01@gmail.com',
     description='A small example package for movies recommendation',
-    long_description=long_description,
+    long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type='text/markdown',
-    packages=[SRC_REPO],  # <-- Use find_packages instead of manually specifying list
+    packages=find_packages(),  # ✅ This finds all valid packages automatically
+    install_requires=['streamlit', 'requests', 'pandas', 'numpy', 'scikit-learn', 'nltk'],
     python_requires='>=3.7',
-    install_requires=LIST_OF_REQUIREMENTS,
 )
+
